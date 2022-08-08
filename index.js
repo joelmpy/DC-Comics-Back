0cd41cc589228264f8dc0e11f9c866bbcdc7b34e
Joel <joel_mpy@hotmail.com>
2022-08-08 18:30:10 +0200

    update

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.get("/hero/:slug", (req, res, next) => {
     });
 });
 
-app.listen(port || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("listen my port", port);
 });
